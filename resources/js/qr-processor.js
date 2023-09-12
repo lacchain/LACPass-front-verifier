ec4a0d2595d714c7b817226678b43435ec9390fc
eum602 <eum602@gmail.com>
2023-09-12 09:54:59 -0500

    add views to render decoded data

--- resources/js/qr-processor.js
+++ resources/js/qr-processor.js
@@ -37,6 +37,22 @@ window.onload = function () {
       shc: "Smart Health Card standard",
       eu_dcc: "EU DCC standard",
       national: "National interoperability",
+      nameLabel: "Name",
+      dateOfBirthLabel: "Date of birth",
+      genderLabel: "Gender",
+      documentNumberLabel: "Doc. Number",
+      vaccinationDetailsLabel: "Vaccination Details",
+      certificateIdentifierLabel: "Certificate Identifier",
+      certificateIssuerLabel: "Issuer",
+      vaccineDetailsLabel: "Vaccine Details",
+      productNameLabel: "Product Name",
+      atcCodeLabel: "ATC Code",
+      vaccinationCentreLabel: "Vaccination Centre",
+      numberOfDosesLabel: "Number of Doses",
+      dateOfVaccinationLabel: "Date of vaccination",
+      batchNumberLabel: "Batch Number",
+      identificationDataLabel: "Identification Data",
+      viewDetailsText: "View Details",
     },
     es: {
       next: "Siguiente ›",
@@ -59,15 +75,28 @@ window.onload = function () {
       shc: "Estándar Smart Health Card",
       eu_dcc: "Estándar EU DCC",
       national: "Interoperabilidad nacional",
+      nameLabel: "Nombre",
+      dateOfBirthLabel: "Fecha de nac.",
+      genderLabel: "Género",
+      documentNumberLabel: "Nº de doc.",
+      vaccinationDetailsLabel: "Datos de la vacunación",
+      certificateIdentifierLabel: "Identificador del certificado",
+      certificateIssuerLabel: "Emisor",
+      vaccineDetailsLabel: "Datos de la vacuna",
+      productNameLabel: "Nombre del producto",
+      atcCodeLabel: "ATC Code",
+      vaccinationCentreLabel: "Centro de Vacunación",
+      numberOfDosesLabel: "Número de dosis",
+      dateOfVaccinationLabel: "Fecha de vacunación",
+      batchNumberLabel: "Número de Lote",
+      identificationDataLabel: "Datos de identificación",
+      viewDetailsText: "Ver Detalles",
     },
   });
 
   // var resultContainer = document.getElementById("qr-reader-results");
   var lastResult,
     countResults = 0;
-  // lastMessage,
-  // codeId = 0;
-
   function renderResults(decodedText) {
     let compatibility = "";
     if (
@@ -110,6 +139,17 @@ window.onload = function () {
     $("#none").prop("checked", false);
     $("#divoc").prop("checked", false);
 
+    const items = [
+      "no-vaccination-standard-item",
+      "who-ddcc-standard-item",
+      "divoc-standar-item",
+      "smart-health-card-standard-item",
+      "eu-dcc-item",
+      "national-interoperability-item",
+    ];
+
+    let itemsToRemove = [];
+
     switch (compatibility) {
       case "DIVOC":
         $("#divoc").prop("checked", true);
@@ -118,6 +158,8 @@ window.onload = function () {
       case "EU-DCC":
         $("#eu-dcc").prop("checked", true);
         $("#national").prop("checked", true);
+        $("#eu-dcc-item").removeClass("hide").addClass("showx");
+        itemsToRemove = items.filter((item) => item !== "eu-dcc-item");
         break;
       case "National":
         $("#national").prop("checked", true);
@@ -126,17 +168,49 @@ window.onload = function () {
         $("#shc").prop("checked", true);
         $("#national").prop("checked", true);
         break;
-        break;
       default:
         $("#none").prop("checked", true);
         break;
     }
 
+    itemsToRemove.map((item) => {
+      var element = document.getElementById(item);
+      element.parentNode.removeChild(element);
+    });
     $("#step1").removeClass("showx").addClass("hide");
     $("#result").addClass("showx").show();
+
+    /**render modal */
+    // Get the modal
+    const modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    const btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function () {
+      modal.style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+    /** end render modal */
+    /**activate modal */
+    modal.style.display = "block";
   }
   function onScanSuccess(decodedText, decodedResult) {
-    console.log("onScanSuccess ....");
     if (html5QrcodeScanner.getState() !== Html5QrcodeScannerState.NOT_STARTED) {
       // Add this check to ensure success callback is not being called
       // from file based scanner.
@@ -144,19 +218,6 @@ window.onload = function () {
       // Pause on scan result
       html5QrcodeScanner.pause();
     }
-    /**
-     * If you following the code example of this page by looking at the
-     * source code of the demo page - good job!!
-     *
-     * Tip: update this function with a success callback of your choise.
-     */
-    // if (lastMessage !== decodedText) {
-    //   lastMessage = decodedText;
-    //   console.log("decoded Text", decodedText);
-    //   console.log("decoded Result", decodedResult);
-    //   // printScanResultPretty(codeId, decodedText, decodedResult);
-    //   ++codeId;
-    // }
 
     if (decodedText !== lastResult) {
       console.log("decoded Text", decodedText);
@@ -201,6 +262,10 @@ window.onload = function () {
     rememberLastUsedCamera: false,
   });
   html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+  // const decodedText =
+  //   "HC1:6BF1.N3DBSNAP53-OPX:5I Q-N0V0PUF247V.WM3G5GRRDN1:N9WC4/E2ZL7-/T.MN6.9RN79%2O72NB0XY6$BLZ$4R92OTPP74EE4H*ORW7:0P5IORZ69T4*U4CYS+D3MZ8J5C5XQ5VP7ES9UU:A3YZR%3LOU9TKRE8V+4O-3WAX24.MQ*6JRH.0HB.2*IBOSE%NLTFIW G:3577UX-3F7H%:D7L2SP3KXS0CHQI0VC3ZLC%O6I9U8M4R81U GFDOHWHKEFXBG+LRGV6EE5SYAFGI JIVISLKHB58.YN/L8B1ALO4.O20FJGYK7:B3/Q+RHZ97$QUG45:WV2 3UK011JZ42+SU8EFF+1/1J%AJT4OZQ0UGFNDIT9IXR9.63/NB6 1I%D488+7BJO309A.9T7D9R9GQ-SRWQ9GRHF7MSUX.BFDWO/OP9F0YGO6A/%N*N0YNCN*L*K6/IS$LC/98D2095I3KEDKJN%S.T6*B8 TUT99K%82WQN170PHWOQ7OAM0GI74YB0FB47CWAM2DQEM-DNH2QX5MQTT%8EVG55G9RT10JW-H6%L3Z8EMK:$I2CI%$P6KT9*8F68OL5QAH+CC RV21V QEC3K74K$S0WTN3UU+U9W5C-NO8D8A7JEWABQFAOK.GAYLVB31E56YLG4H9INAQA5T76MJUR%NJZH/A7%ODS7RFVEAB8-75GE4XQA9KA7WGZ*3+8O6EW:EJR/RO*9. ETA3933VN9ZQQAEC94NJLT2AQOCDF0TAGBF:192S45VG3AT 3FB1O/UO4RC 6B6RC$2.XO9IM5/NR.IFNCB-7AQP*FJSLJS+RF2TQUBHVH1QUQ K5H2L6AC.1SAVA*AFYDIVJZBOZCR*9F-UUFXRENHFD33P7/9US6TB+TWHROW6FPV4%4 IB*PN964Y6UJ:O1M39RBWWR/:5*:NNC39JN0:7ML9VHIL.NNOSQ2IQTK*8J-7RYNR4%TQEH82CY%NP/U*HOFU1A5GFP8%$MJ:PRUR9YEXXU4RHB+71GFXFC92VEZEDXN5%V$IQALQ3ATTG1N 7HNE3:E477GJQ-/3UI6T.6PC8*GC$KMNX9 /O-8SE+1M 0 P1K2";
+  // renderResults(decodedText);
 
   $("#button1").click(function () {
     $("#result").removeClass("showx").addClass("hide");
